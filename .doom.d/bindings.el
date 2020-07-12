bf91b5c4e2dbf063d11dd0564cf529b71d1aca74
ms747 <mayur.shah.ha@gmail.com>
2020-07-12 11:07:53 +0530

    Add Emacs config

---
+++ .doom.d/bindings.el
@@ -0,0 +1,6 @@
+;;; ~/.doom.d/bindings.el -*- lexical-binding: t; -*-
+
+(define-key evil-insert-state-map (kbd "C-j") 'evil-force-normal-state)
+(define-key evil-insert-state-map (kbd "C-k") 'evil-force-normal-state)
+(define-key evil-insert-state-map (kbd "C-h") 'evil-force-normal-state)
+(define-key evil-insert-state-map (kbd "C-l") 'evil-force-normal-state)
