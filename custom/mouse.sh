2c1372b46dd5265ca0356ee422f63126f0fed1e7
ms747 <mayur.shah.ha@gmail.com>
2021-03-30 14:41:20 +0530

    Properly disable mouse accelaration

--- custom/mouse.sh
+++ custom/mouse.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-xinput --set-prop 9 "libinput Accel Speed" -0.9
+xinput --set-prop 9 "libinput Accel Speed" -0.5
+xinput --set-prop 9 'libinput Accel Profile Enabled' 0, 1
 xset m 0/1 4
